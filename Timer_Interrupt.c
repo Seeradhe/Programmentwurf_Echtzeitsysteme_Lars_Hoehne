a010f491c54167ed5021dd6df90b7e3d472c0737
Seeradhe <lars.hoehne2000.de>
2023-05-13 20:16:33 +0200

    Implemented Timer 3 and 5 functionality for PWM and Interrupt

--- Timer_Interrupt.c
+++ Timer_Interrupt.c
@@ -2,53 +2,117 @@
 #include "Drivers.h"
 #include "Timer_Interrupt.h"
 	
-	int InterruptStatusBit = 0;
+	uint8_t InterruptStatusBit = 0;
+	uint8_t RegelbetriebStatusBit = 0;
+	uint8_t BrennerstartStatusBit = 0;
+	
+	void Timer3_Init(void){
+		RCC->APB1ENR |= 0x2; // Clock enable
+		
+		TIM3->PSC = 0x3E80;
+		TIM3->ARR = 500;
+		
+		TIM3->DIER |= 0x0001;
+		
+		TIM3->CR1 |= 0x1;
+		NVIC_EnableIRQ(TIM3_IRQn);
+	}
 	
 	void Timer4_Init(void){
-	// Timer4 Init --> used to determine the length of a button press
 		RCC->APB1ENR |= 0x4;
 	
-		TIM4->PSC |= 0x7FFF;
-		TIM4->ARR |= 100000;
+		TIM4->PSC = 0x03E8;		// every increment in the cnt register is 100 us
+		TIM4->ARR = 100;			// 10 ms Period
 
+		/*
+			The following 4 lines implement the basic PWM functionality, and determine the PWM mode,
+			the direction of counting, which channel of the timer is used for comparison and the DutyCycle
+			for more information look up 18.3.9 "PWM Mode" in the reference manual for the STM32F407VGT6 on
+			page 610
+		*/
+		TIM4->CCMR1 |= 0x0060;
+		TIM4->CCR1 = 10;
+		TIM4->CCER |= 0x0001;
+		TIM4->CR1 |= 0x0080;
+		
+		TIM4->EGR |= 0x0001;	// bit to transfer the configuration of the previous lines of code into the shadow registers
+		
+		TIM4->CR1 |= 0x0001;	// start the timer to enable the PWM signal
+	}
+		
+	void Timer5_Init(void){
+		// Timer 5 Init --> used to determine how long the button was pressed to determine the different input signals
+		RCC->APB1ENR |= 0x8;
+	
+		TIM5->PSC = 0x3E80;		// every increment in the cnt register is a ms
+		TIM5->ARR = 100000;		// the maximum length of a button press (100s) before the counter resets
 	}
 		
 	void User_Button_Interrupt_Init(void){
 	// Interrupt Init --> used to listen for the Press of the User Button on the STM32
-	EXTI->IMR |= 0x1;
-	EXTI->RTSR |= 0x1;
-	EXTI->FTSR |= 0x1;
+	EXTI->IMR |= 0x1;				// enable the interrupt on line 0
+	EXTI->RTSR |= 0x1;			// enable falling edge interrupt detection for line 0
+	EXTI->FTSR |= 0x1;			// enable rising edge interrupt detection for line 0
 	
-	NVIC_EnableIRQ(EXTI0_IRQn);
+	NVIC_EnableIRQ(EXTI0_IRQn);		// enable the Interrupt on line 0
 	}
 	
-	void actOnTimer(uint32_t counter){
+	void ButtonInputAction(uint32_t msButtonPressLength){
 
-	if(counter> 0 && counter< SHORTBUTTONPRESS){
+	if(msButtonPressLength> 0 && msButtonPressLength< SHORTBUTTONPRESS){
 		// Leistungsvorgabe + 10%, Overflow von 100% auf 0%
-		toggle_Pin(GPIOD, 12);
+		if(TIM4->CCR1 == 100){
+			TIM4->CCR1 = 0;
+		}
+		else{
+			TIM4->CCR1 += 10;
+		}
 	}
-	else if(counter> SHORTBUTTONPRESS && counter< REGULARBUTTONPRESS){
+	else if(msButtonPressLength> SHORTBUTTONPRESS && msButtonPressLength< REGULARBUTTONPRESS){
 		// toggelt Signal Regelbetrieb
-		toggle_Pin(GPIOD, 13);
+		if(RegelbetriebStatusBit == 0){
+			RegelbetriebStatusBit = 1;
+		}
+		else{
+			RegelbetriebStatusBit = 0;
+		}
 	}
-	else if (counter > LONGBUTTONPRESS){
+	else if (msButtonPressLength > LONGBUTTONPRESS){
 		// toggelt Signal Brennerstart
-		toggle_Pin(GPIOD, 14);
+		if(BrennerstartStatusBit == 0){
+			BrennerstartStatusBit = 1;
+		}
+		else{
+			BrennerstartStatusBit = 0;
+		}
 	}
 }
 	
+/*
+	Pre-Processor directive removes the warning for the IRQHandlers that exists because of the
+	initial decleration as weak --> warnings are not an issue but a result of the code structure
+	within Keil and can not lead to an error
+*/
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wmissing-prototypes"
+	void TIM3_IRQHandler(void){
+		// toggles the Watchdog Signal every 500ms
+		toggle_Pin(GPIOD, 15);
+		TIM3->SR &= 0xFFFE;
+	}
+
 	void EXTI0_IRQHandler(void){
+		// Button interrupt
 	NVIC_DisableIRQ(EXTI0_IRQn);
 	
 	if(InterruptStatusBit == 0){
-		TIM4->CR1 |= 0x1;
+		TIM5->CR1 |= 0x1;
 		InterruptStatusBit = 1;
 	}
-	else if(InterruptStatusBit == 1 && (GPIOA->IDR & 0x1) == 0){
-		TIM4->CR1 &= 0xFFFE;
-		actOnTimer(TIM4->CNT);
-		TIM4->CNT &= 0;
+	else{
+		TIM5->CR1 &= 0xFFFE;
+		ButtonInputAction(TIM5->CNT);
+		TIM5->CNT &= 0;
 		InterruptStatusBit = 0;
 	}
 	for(int i = 0; i < 50000; i++);
@@ -56,3 +120,4 @@
 	EXTI->PR |= 0x1;
 	NVIC_EnableIRQ(EXTI0_IRQn);
 }
+#pragma GCC diagnostic pop
--- Timer_Interrupt.h
+++ Timer_Interrupt.h
@@ -1,15 +1,33 @@
 #include "stm32f407xx.h"
 
-#pragma once
+#define SHORTBUTTONPRESS 500
+#define REGULARBUTTONPRESS 1500
+#define LONGBUTTONPRESS 1500
 
-#define SHORTBUTTONPRESS 125
-#define REGULARBUTTONPRESS 375
-#define LONGBUTTONPRESS 375
+extern uint8_t InterruptStatusBit;
+extern uint8_t RegelbetriebStatusBit;
+extern uint8_t BrennerstartStatusBit;
 
-extern int InterruptStatusBit;
-
+/*
+*	\brief Init function that sets basic registers for the Timer 3, Timer 3 is used to control the Watchdog Timer
+*/
+void Timer3_Init(void);
+/*
+*	\brief Init function that sets basic registers for the Timer 4, Timer 4 is used to control the Signal "Soll-Drehzahl"
+*/
 void Timer4_Init(void);
-
+/*
+*	\brief Init function that sets basic registers for the Timer 5, Timer 5 is used to determine the length of a button press on PA0
+*/
+void Timer5_Init(void);
+/*
+*	\brief Init function that sets up the registers for the Interrupt on PA0
+*/
 void User_Button_Interrupt_Init(void);
-
-void actOnTimer(uint32_t counter);
+/*
+*	\brief Function that gets called within the button interrupt function after a falling edge to determine the action
+*				 after a button press of a given length
+*
+* \param msButtonPressLength length of the button press in ms - though the duration is determined by the timer setup
+*/
+void ButtonInputAction(uint32_t msButtonPressLength);
