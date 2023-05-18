a3cb32df3870be6f035588a0515c8d8c043918cc
Seeradhe <lars.hoehne2000.de>
2023-05-18 15:04:24 +0200

    Implemented a loop function that the main executes

--- main_include.c
+++ main_include.c
@@ -7,4 +7,12 @@ void Setup(void){
 	Timer5_Init();
 	User_Button_Interrupt_Init();
 	LED_Init();
+	sleep_ms(100);
+}
+
+__attribute__((noreturn)) void Loop(void){
+	while(1){
+		Statemachine();
+		sleep_ms(100);
+	}
 }
--- main_include.h
+++ main_include.h
@@ -1,9 +1,15 @@
 #include "Timer_Interrupt.h"
 #include "Drivers.h"
+#include "statemachine.h"
 
 #pragma once
 /*
 * \brief Contains all the required Setups from the different .h files to have a simpler viewport in the
-*				 .main file
+*				 main.c file
 */
 void Setup(void);
+/*
+* \brief Contains all the required Functions to be executed periodically to have a simpler viewport in the
+*				 main.c file
+*/
+__attribute__((noreturn)) void Loop(void);
