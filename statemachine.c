24d6fdcf85df74caad303b8e91df49747eefa56c
Seeradhe <lars.hoehne2000.de>
2023-05-18 12:33:54 +0200

    Initial implementation of the statemachine

--- statemachine.c
+++ statemachine.c
@@ -1,2 +1,46 @@
 #include "statemachine.h"
+#include "Timer_Interrupt.h"
+#include "Drivers.h"
 
+uint8_t CurrentState = 0;
+
+void Statemachine(void){
+	
+	switch(CurrentState){
+		
+		// Grundzustand (Brenner aus)
+		case 0:{
+			if(BrennerstartStatusBit == 1){
+				CurrentState = 1;
+			}
+			break;
+		}
+		// Vorlueften
+		case 1:{
+			set_Pin(GPIOD, 13);
+			// wait 5 seconds
+			reset_Pin(GPIOD, 13);
+			CurrentState = 2;
+			break;
+		}
+		// Zuendstellung
+		case 2:{
+			// mindestens 3s warten
+			if(RegelbetriebStatusBit == 1){
+				CurrentState = 3;
+			}
+			break;
+		}
+		// Regelbetrieb
+		case 3:{
+			
+			break;
+		}
+		// Fehler --> Gehe zurueck in Grundzustand
+		default:{
+			CurrentState = 0;
+			break;
+		}
+	}
+	
+}
---
+++ statemachine.h
@@ -0,0 +1,5 @@
+#include "stm32f407xx.h"
+
+extern uint8_t CurrentState;
+
+void Statemachine(void);
